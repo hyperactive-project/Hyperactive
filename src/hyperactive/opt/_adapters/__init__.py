d2ad8db9ce2ab070acb48733a7b1b9702d27a95d
Simon Blanke <simon.blanke@yahoo.com>
2025-12-14 05:19:59 +0100

    add _BaseScipyAdapter

--- src/hyperactive/opt/_adapters/__init__.py
+++ src/hyperactive/opt/_adapters/__init__.py
@@ -1,7 +1,9 @@
 """Adapters for individual packages."""
+
 # copyright: hyperactive developers, MIT License (see LICENSE file)
 
 from ._base_optuna_adapter import _BaseOptunaAdapter
 from ._gfo import _BaseGFOadapter
+from ._base_scipy_adapter import _BaseScipyAdapter
 
-__all__ = ["_BaseOptunaAdapter", "_BaseGFOadapter"]
+__all__ = ["_BaseOptunaAdapter", "_BaseGFOadapter", "_BaseScipyAdapter"]
