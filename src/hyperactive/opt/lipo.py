1d1087f35918eda603ceade8ffca07121dd8a069
Direk Kakkar <millioniar1717@gmail.com>
2026-03-23 23:49:06 +0530

    `__init__()`

--- src/hyperactive/opt/lipo.py
+++ src/hyperactive/opt/lipo.py
@@ -1,2 +1,10 @@
 import numpy as np
 from lipo import GlobalOptimizer
+
+class LIPOOptimizer:
+    """Parameter-free global optimizer via the lipo package."""
+    def __init__(self, search_space, n_iter, experiment, maximize=True):
+        self.search_space = search_space
+        self.n_iter = n_iter
+        self.experiment = experiment
+        self.maximize = maximize
