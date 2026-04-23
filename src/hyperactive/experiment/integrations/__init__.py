3a544ae6745bab23ae29a582506b03937343aa55
Simon Blanke <simon.blanke@yahoo.com>
2026-04-23 20:48:20 +0200

    remove wrong import

--- src/hyperactive/experiment/integrations/__init__.py
+++ src/hyperactive/experiment/integrations/__init__.py
@@ -1,10 +1,8 @@
 """Integrations with packages for tuning."""
+
 # copyright: hyperactive developers, MIT License (see LICENSE file)
 
 from hyperactive.experiment.integrations.lightgbm_experiment import LightGBMExperiment
-from hyperactive.experiment.integrations.skforecast_forecasting import (
-    SkforecastExperiment,
-)
 from hyperactive.experiment.integrations.sklearn_cv import SklearnCvExperiment
 from hyperactive.experiment.integrations.skpro_probareg import (
     SkproProbaRegExperiment,
@@ -21,7 +19,6 @@ from hyperactive.experiment.integrations.torch_lightning_experiment import (
 
 __all__ = [
     "LightGBMExperiment",
-    "SkforecastExperiment",
     "SklearnCvExperiment",
     "SkproProbaRegExperiment",
     "SktimeClassificationExperiment",
